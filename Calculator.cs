afc7d7e4ee4bbb19b316654a407f4725f8c6cf6e
nana-syarifuddin <nana.informatika@students.amikom.ac.id>
2022-03-16 21:59:39 +0700

    Tugas 4

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,14 @@ class Calculator
 {
 	static void Main(String[] args)
 	{
-	
+		int a = 10;
+		int b = 6;
+		
+		Console.Writeline("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+		Console.Writeline("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+		
+		Console.Writeline("\nTekan sembarang key untuk keluar");
+		Console.ReadKey();
 	}
 	
 	static void Penambahan(int a, int b)
